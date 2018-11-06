340e5799322bccc92706f2f03981ee6ab8728420
Marc Mills <mdmills1990@gmail.com>
2018-11-06 22:32:52 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,24 +1,17 @@
 class Application
-
+ 
   def call(env)
     resp = Rack::Response.new
-
-    num_1 = Kernel.rand(1..2)
-    num_2 = Kernel.rand(1..2)
-    num_3 = Kernel.rand(1..2)
-
-    resp.write "#{num_1}\n"
-    resp.write "#{num_2}\n"
-    resp.write "#{num_3}\n"
-
-
-    if num_1 == num_2 && num_2 == num_3
-      resp.write "You Win"
+    req = Rack::Request.new(env)
+ 
+    if req.path=="/songs"
+      resp.write "You requested the songs"
     else
-      resp.write "You Lose"
+      resp.write "Route not found"
+      resp.status = 404
     end
+ 
     resp.finish
   end
-
-
+end
 end
